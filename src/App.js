d2175b49465b93f589c678ab302bda0e87deebde
golecl <laura.golec@gmail.com>
2023-09-05 21:07:38 +0100

    added extremely basic placeholders for navbar, footer, and menu

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.css';
-import Home from './pages/home/Home.jsx';
-import About from './pages/about/About.jsx';
-import Contact from './pages/contact/Contact.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import Home from "./pages/home/Home.jsx";
+import About from "./pages/about/About.jsx";
+import Contact from "./pages/contact/Contact.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Menu from "./components/menu/Menu.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
-import {
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createHashRouter, RouterProvider, Outlet } from "react-router-dom";
 
-function App (){
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
   const router = createHashRouter([
     {
       path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/contact",
-      element: <Contact />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
     },
   ]);
-  
-  
 
-  return (
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <React.StrictMode>
-        <RouterProvider router={router} />
-      </React.StrictMode>
-    )
+  return ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Menu.css";
+
+function Menu() {
+  return <div className="menu">Menu</div>;
+}
+
+export default Menu;
