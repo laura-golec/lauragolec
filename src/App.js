d1cd435cd884e6aec39aeac1ee93ea27b4a19066
golecl <laura.golec@gmail.com>
2023-09-05 20:21:52 +0100

    updated routing tool to work with github pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 import Home from './pages/home/Home.jsx';
 import About from './pages/about/About.jsx';
 import Contact from './pages/contact/Contact.jsx';
 
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 
 function App (){
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       element: <Home />,
@@ -25,7 +26,15 @@ function App (){
     },
   ]);
   
-  return <RouterProvider router={router} />;
+  
+
+  return (
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    )
+  );
 }
 
 export default App;
