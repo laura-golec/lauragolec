c2f1620f36260a954ff99ae4d2830d1fee00c575
Laura Golec <laura.golec@gmail.com>
2023-09-14 18:29:44 +0100

    added content to about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -22,49 +22,144 @@ function About() {
     const blob4Position = getRandomPosition();
 
     const Profile = () => (
-            <div className="blob-containerAbout">
-                <img src={ProfilePic} className="blobAbout" style={{ height: '18vw', borderRadius: '50%', zIndex: '8', top: '15%', left: '20%' }} />
-                <img
-                    src={BlobOneAbout}
-                    alt="Blob One"
-                    className="blobAbout blob-1About"
-                    style={{ '--x': `${blob1Position.x}%`, '--y': `${blob1Position.y}%` }}
-                />
-                <img
-                    src={BlobTwoAbout}
-                    alt="Blob Two"
-                    className="blobAbout blob-2About"
-                    style={{ '--x': `${blob2Position.x}%`, '--y': `${blob2Position.y}%` }}
-                />
-                <img
-                    src={BlobThreeAbout}
-                    alt="Blob Three"
-                    className="blobAbout blob-3About"
-                    style={{ '--x': `${blob3Position.x}%`, '--y': `${blob3Position.y}%` }}
-                />
-                <img
-                    src={BlobFourAbout}
-                    alt="Blob Four"
-                    className="blobAbout blob-4About"
-                    style={{ '--x': `${blob4Position.x}%`, '--y': `${blob4Position.y}%` }}
-                />
-            </div>
+        <div className="blob-containerAbout">
+            <img src={ProfilePic} className="blobAbout" style={{ height: '18vw', borderRadius: '50%', zIndex: '8', top: '15%', left: '20%' }} />
+            <img
+                src={BlobOneAbout}
+                alt="Blob One"
+                className="blobAbout blob-1About"
+                style={{ '--x': `${blob1Position.x}%`, '--y': `${blob1Position.y}%` }}
+            />
+            <img
+                src={BlobTwoAbout}
+                alt="Blob Two"
+                className="blobAbout blob-2About"
+                style={{ '--x': `${blob2Position.x}%`, '--y': `${blob2Position.y}%` }}
+            />
+            <img
+                src={BlobThreeAbout}
+                alt="Blob Three"
+                className="blobAbout blob-3About"
+                style={{ '--x': `${blob3Position.x}%`, '--y': `${blob3Position.y}%` }}
+            />
+            <img
+                src={BlobFourAbout}
+                alt="Blob Four"
+                className="blobAbout blob-4About"
+                style={{ '--x': `${blob4Position.x}%`, '--y': `${blob4Position.y}%` }}
+            />
+        </div>
     )
 
     return (
         <div className='about' justifyContent='center'>
-            <div style={{ display: 'flex', alignContent:'center', justifyContent:'center', flexDirection:'column', justifySelf:'center!important'}}>
-                    <Profile />
-                <Typography variant='h3' sx={{ backgroundColor: 'var(--mainbg)', color: 'var(--accent)', paddingTop: '2vh', textAlign: 'center'}}>
+            <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', flexDirection: 'column', justifySelf: 'center!important' }}>
+                <Profile />
+                <Typography variant='h3' sx={{ backgroundColor: 'var(--mainbg)', color: 'var(--accent)', paddingTop: '2vh', textAlign: 'center' }}>
                     About Me
                 </Typography>
             </div>
 
-            <div className='aboutContent'>
-                <Typography variant='body1'>
-                    I am a 4th year Trinity Computer Science student in the Masters Programme of my course.
+            <div className='aboutContent' style={{ maxWidth: '800px' }}>
+                <Typography variant='body1' >
+                    Hi, I am Laura Golec! I am a 4th year Trinity Computer Science student in the Masters Programme of my course.
+                    I am currently seeking a 6 month software engineering internship at the beginning of 2024 as part of my course.
 
+                    <br />
+                    <br />
+
+                    I have a particular interest in frontend development and love coherent and maintainable frontend code using the most modern standards.
+
+                    <br />
+                    <br />
                 </Typography>
+                <Typography variant='h5' sx={{ borderBottom: 'double 2px var(--fadedcolour)' }}>
+                    Education
+                </Typography>
+                <div style={{ margin: '0 1vw' }}>
+                    <div style={{ display: 'flex', direction: 'row', borderBottom: 'dotted 1px var(--fadedcolour)', justifyContent: 'space-between' }}>
+                        <Typography variant='h6' sx={{ width: 'fit-content', fontWeight: '600' }}>
+                            Tertiary: <i>Trinity College Dublin</i>
+                        </Typography>
+                        <Typography variant='h6' sx={{ width: 'fit-content' }}>
+                            Sept. 2020 - Present
+                        </Typography>
+                    </div>
+                    <Typography variant='body1' sx={{ borderBottom: 'dotted 1px var(--fadedcolour)', fontWeight: '600' }}>
+                        <i>Bachelor in Computer Science</i>
+                    </Typography>
+                    <Typography variant='body1' sx={{ borderBottom: 'solid 1px var(--fadedcolour)', fontWeight: '500' }}>
+                        Upper Second Class Honours in Third Year
+                        <br />
+                        Upper Second Class Honours in Second Year
+                        <br />
+                        First Class Honours in First Year (Book Prize)
+                    </Typography>
+
+                    <div style={{ display: 'flex', direction: 'row', borderBottom: 'dotted 1px var(--fadedcolour)', justifyContent: 'space-between' }}>
+                        <Typography variant='body1' sx={{ width: 'fit-content', fontWeight: '600' }}>
+                            Secondary: <i>Adamstown Community College</i>
+                        </Typography>
+                        <Typography variant='body1' sx={{ width: 'fit-content' }}>
+                            Sept. 2015 - June 2020
+                        </Typography>
+                    </div>
+                </div>
+
+                <br />
+                <Typography variant='h5' sx={{ borderBottom: 'double 2px var(--fadedcolour)' }}>
+                    Skills
+                </Typography>
+                <div style={{ margin: '0 1vw' }}>
+                    <Typography variant='h6' sx={{ width: 'fit-content', fontWeight: '600' }}>
+                        Languages
+                    </Typography>
+                    <Typography variant='body1' sx={{ borderBottom: 'solid 1px var(--fadedcolour)', fontWeight: '500' }}>
+                        English - <i>Fluent</i>
+                        <br />
+                        Polish - <i>Fluent</i>
+                    </Typography>
+
+                    <Typography variant='h6' sx={{ width: 'fit-content', fontWeight: '600' }}>
+                        Programming
+                    </Typography>
+                    <Typography variant='body1' sx={{ borderBottom: 'solid 1px var(--fadedcolour)', fontWeight: '500' }}>
+                        JavaScript
+                        <br />
+                        Python
+                        <br />
+                        Java
+                        <br/>
+                        C
+                        <br/>
+                        HTML5
+                        <br/>
+                        CSS
+                        <br/>
+                        SQL
+                        <br/>
+                    </Typography>
+
+                    <Typography variant='h6' sx={{ width: 'fit-content', fontWeight: '600' }}>
+                        Software
+                    </Typography>
+                    <Typography variant='body1' sx={{ borderBottom: 'solid 1px var(--fadedcolour)', fontWeight: '500' }}>
+                        React
+                        <br />
+                        Vue.js
+                        <br />
+                        Github
+                        <br/>
+                        Gitlab
+                        <br/>
+                        Visual Studio Code
+                        <br/>
+                        React Native
+                        <br/>
+                        MySQL
+                        <br/>
+                    </Typography>
+                </div>
             </div>
         </div>
     )
